7d738ef974894d7e16e6f401bdf83b2f733ae8aa
SlhBtmz <152915818+SlhBtmz@users.noreply.github.com>
2024-01-28 17:18:12 +0300

    For

---
+++ for.py
@@ -0,0 +1,9 @@
+liste = ["Istanbul","Ankara","Giresun","Nevşehir","Antalya","Mugla","Hatay"]
+
+
+#for il in liste:
+#    print ("Şu an listedeki il: " + il)    
+#
+for i in range(len(liste)):
+    print ("Şu an listedeki il: " + liste[i])
+
