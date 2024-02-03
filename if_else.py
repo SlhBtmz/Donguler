706ade3a2f48f479f9d0086ec5be2a9659f698d3
SlhBtmz <152915818+SlhBtmz@users.noreply.github.com>
2024-02-03 19:41:20 +0300

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,10 @@
+num = int(input("Please enter a number: "))
+
+if num > 0:
+    print(f"{num} is a positive number")
+elif num == 0:
+    print (f"{num} is zero")
+else: 
+    (print (f"{num} is a negatice number"))
+
+
