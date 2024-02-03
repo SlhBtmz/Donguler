a06a641fd885eae816418b209ac10900a524805a
SlhBtmz <152915818+SlhBtmz@users.noreply.github.com>
2024-02-03 19:41:43 +0300

    Add files via upload

---
+++ if_elif_else_factorial.py
@@ -0,0 +1,10 @@
+notes = {"ahmet":58, "mehmet":76, "ebru":44, "pinar":900}
+
+for key, value in notes.items():
+    if value >=50:
+        print(f"{key} aldigi not {value} : GECTİ")
+    else:
+        print(f"{key} aldigi not {value} : KALDİ")
+
+
+    
